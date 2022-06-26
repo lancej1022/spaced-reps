375052155590897debe6cea551ffb0c08035c17a
lancej1022 <lancej1022@gmail.com>
2022-06-26 00:05:00 -0700

    style(cleanup): delete unused components. delete unnecessary cy.visit

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,6 @@ import 'cypress-axe';
 
 describe('empty spec', () => {
   it('passes a11y check on initial render', () => {
-    // cy.visit('/popup.html');
     cy.checkA11y();
   });
 });
--- src/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <button>Add reminder</button>
-    </footer>
-  );
-}
