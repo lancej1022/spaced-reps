9ad37ab053b6fc7de927a8c7ba926e342e7d5786
lancej1022 <lancej1022@gmail.com>
2022-08-28 17:07:55 -0700

    update heap implementation

--- algoSolutions.js
+++ algoSolutions.js
@@ -474,53 +474,44 @@ class MaxHeap {
 }
 
 class Heap {
-  constructor(array, compareFunc) {
-    this.compareFunc = compareFunc;
+  constructor(array, compare) {
+    this.compare = compare;
     this.heap = this.buildHeap(array);
     this.length = this.heap.length;
   }
 
-  swap(i, j, heap) {
-    let temp = heap[j];
-    heap[j] = heap[i];
-    heap[i] = temp;
-  }
-
   buildHeap(array) {
     for (let i = array.length - 1; i >= 0; i -= 1) {
-      this.siftDown(i, array.length - 1, array);
+      this.siftDown(i, array);
     }
     return array;
   }
 
-  siftDown(index, endIndex, heap) {
-    let childOneIndex = index * 2 + 1;
-    while (childOneIndex <= endIndex) {
-      let childTwoIndex = index * 2 + 2 <= endIndex ? index * 2 + 2 : -1;
+  siftDown(parentIndex, heap) {
+    const end = heap.length - 1;
+    let childOneIndex = parentIndex * 2 + 1;
+    while (childOneIndex <= end) {
+      let childTwoIndex = parentIndex * 2 + 2 <= end ? parentIndex * 2 + 2 : -1;
       let indexToSwap;
-      if (
-        childTwoIndex !== -1 &&
-        this.compareFunc(this.heap[childTwoIndex], this.heap[childOneIndex])
-      ) {
+      if (childTwoIndex !== -1 && this.compare(heap[childTwoIndex], heap[childOneIndex])) {
         indexToSwap = childTwoIndex;
       } else {
         indexToSwap = childOneIndex;
       }
 
-      if (this.compareFunc(this.heap[indexToSwap], this.heap[index])) {
-        this.swap(index, indexToSwap);
-        index = indexToSwap;
-        childOndexIndex = index * 2 + 1;
+      if (this.compare(heap[indexToSwap], heap[parentIndex])) {
+        this.swap(parentIndex, indexToSwap, heap);
+        parentIndex = indexToSwap;
+        childOneIndex = parentIndex * 2 + 1;
       } else {
         return;
       }
     }
   }
 
-  siftUp(index, heap) {
+  siftUp(index) {
     let parentIndex = Math.floor((index - 1) / 2);
-    // TODO: this bit might be wrong :|
-    while (parentIndex >= 0 && this.compareFunc(this.heap[index], this.heap[parentIndex])) {
+    while (parentIndex >= 0 && this.compare(this.heap[index], this.heap[parentIndex])) {
       this.swap(index, parentIndex, this.heap);
       index = parentIndex;
       parentIndex = Math.floor((index - 1) / 2);
@@ -529,20 +520,26 @@ class Heap {
 
   insert(val) {
     this.heap.push(val);
-    this.siftUp(this.heap.length - 1, this.heap);
+    this.siftUp(this.heap.length - 1);
     this.length += 1;
   }
 
   remove() {
     this.swap(0, this.heap.length - 1, this.heap);
     let returnVal = this.heap.pop();
-    this.siftDown(0, this.heap.length - 1, this.heap);
+    this.siftDown(0, this.heap);
     this.length -= 1;
     return returnVal;
   }
 
   peek() {
     return this.heap[0] || null;
+  }
+
+  swap(i, j, heap) {
+    const temp = heap[j];
+    heap[j] = heap[i];
+    heap[i] = temp;
   }
 }
 
